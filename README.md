47f3cdfdfba265b0203847acea4ff12ab869b6b2
Rémi Testa <remi.testa@gmail.com>
2019-07-24 20:53:01 +0200

    release 1.0.0

---
+++ README.md
@@ -0,0 +1,11 @@
+# MacOS Battery Remaining Menubar
+
+A really simple app that displays remaining battery time in the mac menubar.
+
+## Screenshot
+
+![screenshot](screenshot.png)
+
+## Download
+
+[Download the latest release here]()
---
+++ index.js
@@ -0,0 +1,58 @@
+const { menubar } = require("menubar");
+const { exec } = require("child_process");
+const { Menu } = require("electron");
+
+const mb = menubar({
+  tooltip: "Remaining battery",
+});
+
+function getRemainingTime() {
+  return new Promise((resolve, reject) => {
+    exec('pmset -g batt|grep remaining|cut -d" " -f1,5', function(
+      err,
+      stdout,
+      stderr
+    ) {
+      if (stdout) {
+        resolve(String(stdout));
+      } else {
+        reject();
+      }
+    });
+  });
+}
+
+function overrideClick() {
+  mb.tray._events.click = function() {};
+  mb.tray._events["double-click"] = function() {};
+}
+
+function setRightClickMenu() {
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Quit",
+      click: () => {
+        mb.app.quit();
+      }
+    }
+  ]);
+  mb.tray.on("right-click", () => {
+    mb.tray.popUpContextMenu(contextMenu);
+  });
+}
+
+async function updateValue() {
+  const remainingTime = await getRemainingTime();
+  mb.tray.setTitle(remainingTime);
+}
+
+mb.on("ready", () => {
+  overrideClick();
+  setRightClickMenu();
+  updateValue();
+  setInterval(() => {
+    try {
+      updateValue();
+    } catch (e) {}
+  }, 10000);
+});
